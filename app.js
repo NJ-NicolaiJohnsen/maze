8c7812b2889b86524e271be94293feea97bd865c
--replace-all <nico7711@outlook.dk>
2020-06-28 20:30:57 +0200

    starting to create an automated maze

--- app.js
+++ app.js
@@ -1,66 +1,44 @@
-function createWalls(size){
-    let wall = document.querySelector('.wall');
-    let maze = document.querySelector('.maze');
-    let walls = [];
+let x = 0;
+let y = 0;
 
-    for (i=0; i < size; i++) {
-        walls.push(wall.cloneNode());
-    }
-
-    walls.forEach(wall=>{
-        wall.style.display = 'inline-block';
-        wall.style.float = 'left';
-        maze.appendChild(wall);
-    })
-    
-    return walls;
-
-}
-// createWalls(SIZE) should be defined by an input in the html. Something you select, if you get more advanced.
-
-let pinkWallArray = [];
-
-function wallColor(walls) {
-    walls.forEach(wall=>{
-        if (Math.random()*2 > 1) {
-            wall.style.background = 'pink';
-           
-            pinkWallArray.push(wall)
-        }
-    })   
+for (i=1; i<20; i+1){
+    x+=i;
+   
 }
 
-wallColor(createWalls(400));
+console.log(x);
 
 
-pinkWallArray.forEach(pinkWall =>{
-
-    
-
-})
 
 
-let arr1 = {item1: 42,
-        item2: 'another thing',
-        item3: function () {
-            console.log('running!');
-        }
-};
-    
-let arr2 = [13, 24, 32];
 
-let isEqual = function (value, other) {
 
-    let type = Object.prototype.toString.call(value);
 
-    if (type !== Object.prototype.toString.call(other)){
-        return false;
-    }
 
-    return true;
-}
 
-console.log(Object.prototype.toString.call(arr2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 let player = document.querySelector('.player');
 let goal = document.querySelector('.goal');
 let coordinates = {
@@ -107,3 +85,31 @@ document.addEventListener('keyup', function(event){
     console.log(coordinates.x, coordinates.y)
 
 })
+
+
+var c = document.getElementById('myCanvas');
+
+var ctx = c.getContext("2d");
+
+ctx.moveTo(10,0);
+ctx.lineTo(10, 10);
+ctx.stroke();
+
+abc = c.getContext('2d');
+abc.beginPath();
+abc.arc(45, 60, 30, 0, 2 * Math.PI); // x, y, radius, radians missing, 2 * PI = circle 
+abc.stroke();
+
+
+function walltest(){
+    let randomFoo = Math.random()*2;
+    
+    if (randomFoo < 1){
+        console.log('up')
+    } else {
+        console.log('left')
+    }
+}
+walltest()
+
+*/
