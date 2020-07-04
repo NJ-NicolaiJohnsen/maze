0243462c2bb3cab8c0de2c9a55d757b67c76c57c
--replace-all <nico7711@outlook.dk>
2020-07-04 14:03:22 +0200

    there is now a movable player node

--- app-2.js
+++ app-2.js
@@ -4,7 +4,6 @@ function createWallDivs(){
     let maze = document.querySelector('.maze');
     grid = [];
     let rows = []
-    let columns = [];
 
     let height = 25;
     let width = 25;
@@ -47,3 +46,57 @@ function createWallDivs(){
 }
 createWallDivs();
 
+
+function gridData(x, y){
+    let grid = [];
+    let rows = document.querySelectorAll('.row')
+
+    rows.forEach(e=>{
+        grid.push(e.children)
+
+    })
+    return grid[x][y]
+}
+
+
+let xAxis = 0;
+let yAxis = 0;
+gridData(xAxis, yAxis)
+document.addEventListener('keydown', function(event){ //move the player unit
+// Color the node gridNode that i am on pink. Pink is going to be the player unit. If the node i move into is === 1, then dont move. 
+    
+   
+  
+    if (xAxis < 0){xAxis = 0}
+    if (xAxis > 24){xAxis = 24}
+    if (yAxis < 0){yAxis = 0}
+    if (yAxis > 24){yAxis = 24}
+
+    if (event.keyCode === 37){ // left
+        yAxis-=1
+    }
+
+    if (event.keyCode === 38){ // up
+        xAxis-=1;
+    }
+
+    if (event.keyCode === 39){ // right
+        yAxis+=1;
+    }
+
+    if (event.keyCode === 40){ // down
+        xAxis+=1;
+    }
+
+    let grid = gridData(xAxis, yAxis);
+    
+    console.log(xAxis, yAxis)  
+
+    if (grid.style.backgroundColor !== 'blue'){
+        grid.style.backgroundColor = 'pink';
+    }
+
+
+
+    
+})
