e370e10a71be87c877e523c9be9fc9c2b4c244df
--replace-all <nico7711@outlook.dk>
2020-07-08 19:41:08 +0200

    started tracking coordinates

--- app-2.js
+++ app-2.js
@@ -1,7 +1,41 @@
 // The maze is a square, so i only have one size listen, being the height. The height represents the amount of rowDivs the createWallDivs function should create. The width is the amount of columnDivs in the maze. This maze being a square, i only use one value.
+// 1. track the current position, and pass the current position on to the last position, when issuing a move command. Then style all the lastPosition coordinates to green.
+
+// 2. Generate a binary maze
+/*
+function generateBinaryMaze() {
+    // 10 ROWS OF 10 DIVS
+    // 1. go to a div
+    // 2. color either the div to the right blue, or the div below blue
+    // 3. skip a div
+    // 4. repeat
+
+    const currentRow = // get the nth child of the maze
+    const divsYoullUse = // get divs 0, 2, 4, 6, 8 etc
+    for (const div in divsYoullUse) {  // div is 
+        const wallIsToTheRight = Math.random() * 2 > 1
+        if (wallIsToTheRight) {
+            const divToTheRight = currentRow[divIndex + 1]
+            divToTheRight.style.backgroundColor = "blue"
+            recordOfWallCoords.push([index, divIndex + 1])
+        } else {
+            const divBelow = maze.children[index + 1].children[divIndex]
+            divBelow.style.backgroundColor = "blue"
+            recordOfWallCoords.push([index + 1, divIndex])
+        }
+    }
+}
+*/
+
+
 let xAxis = 0;
 let yAxis = 0;
-let height = 40;
+let height = 800/20;
+
+let currentPosition = {x: 0, y: 0}
+let lastPosition = {x: xAxis, y: yAxis}
+
+
 
 function setMazeSize(){
     let maze = document.querySelector('.maze');
@@ -54,18 +88,25 @@ function createWallDivs(){
 }
 createWallDivs();
 
+let gridCoordinates = []
 
-
-function gridData(y, x){
+function gridData(){
     const grid = [];
     const rows = document.querySelectorAll('.row')
-
-    rows.forEach(e=>{
-        grid.push(e.children)
-    })
-
-    return grid[y][x]
+    if (rows) {
+        rows.forEach(e=>{
+            grid.push(e.children)
+            gridCoordinates.push(e.children)
+        })
+    } else {
+        throw "Something went wrong!"
+    }
 }
+gridData()
+
+// if coordinate === 1, not passable
+
+console.log(gridCoordinates)
 
 function moveCommands(){
     let grid = gridData(yAxis, xAxis)
@@ -120,13 +161,13 @@ function moveCommands(){
 
 document.addEventListener('keydown', function(event){ //move the player unit
 // Color the gridNode that i am on pink. Pink is going to be the player unit. If the node i move into is === 1, then dont move.
-    let commands = moveCommands(); //http: rosettacode.org/wiki/Maze_generation#JavaScript
+    let commands = moveCommands(); //http:rosettacode.org/wiki/Maze_generation#JavaScript
 
 
 
     if (xAxis < 0){xAxis = 0}
     if (xAxis > height){xAxis = height}
-    if (yAxis < 0){yAxis = 0}
+    if (yAxis <= 0){yAxis = 0}
     if (yAxis > height){yAxis = height}
 
     if (event.keyCode === 37){ // left
